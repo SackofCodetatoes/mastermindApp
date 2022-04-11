53bd479a52c6adcd62d0be673eec71b3869dcd60
Jason <jsnwong@ucdavis.edu>
2022-04-11 16:13:20 -0700

    implement base requirements for mastermind

--- app/src/main/java/com/example/mastermind/MainActivity.java
+++ app/src/main/java/com/example/mastermind/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.mastermind;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.NumberPicker;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -16,27 +18,32 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
     private NumberPicker picker1, picker2, picker3, picker4;
     private String[] pickerVals;
     private Button tryButton;
-    private TextView textDisplay, attemptsDisplay;
+    private TextView textDisplay, attemptsDisplay, textRecord;
     private RequestQueue queue;
     private int[] secretCode = {1, 2, 3, 4};
-    private String responseString;
-    int attempts = 10, correctDigits = 0, correctPositions = 0, currentInt;
+    private String responseString, result;
+    StringBuilder recordedAttempts = new StringBuilder(500);
+    int attempts = 10, correctDigits = 0, correctPositions = 0;
     private boolean[] secretNums = new boolean[8];
+    LinearLayout record;
+    ScrollView scrollingRecord;
+
     String url = "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new";
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
+        );
         initializeGame();
-        interfaceInit();
 
+        interfaceInit();
     }
 
     void initializeGame(){
@@ -48,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
         for(int i = 0; i < 4; i++){
             secretNums[secretCode[i]] = true;
         }
+        recordedAttempts.setLength(0);
 
     }
 
@@ -95,43 +103,52 @@ public class MainActivity extends AppCompatActivity {
 
         textDisplay = findViewById(R.id.text_main_display);
         textDisplay.setText(("Hello World!"));
-
+        textRecord = findViewById(R.id.text_main_record);
         attemptsDisplay = findViewById(R.id.text_main_attempts);
         attemptsDisplay.setText("Attempts Remaining: " + attempts);
+//        record = findViewById(R.id.layout_main_record);
+
 
         tryButton = findViewById(R.id.button_main_clicker);
         tryButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 int userInput[] = {picker1.getValue(), picker2.getValue(), picker3.getValue(), picker4.getValue()};
-//                int valuePicker1 = picker1.getValue(), valuePicker2 = picker2.getValue(), valuePicker3 = picker3.getValue(), valuePicker4 = picker4.getValue();
-//                String pickerCode = "" + valuePicker1 + valuePicker2 + valuePicker3 + valuePicker4;
-                //game logic here
+
                 correctDigits = 0;
                 correctPositions = 0;
                 //check code
-                for(int i = 0; i < 4; i++){
-                    if(userInput[i] == secretCode[i]){
-                        correctPositions+=1;
+                if(attempts > 0){
+                    for(int i = 0; i < 4; i++){
+                        if(userInput[i] == secretCode[i]){
+                            correctPositions+=1;
+                        }
+                        if(secretNums[userInput[i]]){
+                            correctDigits+=1;
+                        }
                     }
-                    if(secretNums[userInput[i]]){
-                        correctDigits+=1;
+                    if(correctPositions == 4) {
+                        textDisplay.setText("You won! pls program reset");
+                    } else if(correctPositions > 0){
+                        textDisplay.setText("Player guessed a correct number and position. Try again");
+                        result = "one or more correct nums & pos";
+                    } else if(correctDigits > 0) {
+                        textDisplay.setText("Player guessed a correct number. Try again");
+                        result = "one or more correct nums";
+                    } else {
+                        textDisplay.setText("No correct guesses. Try again");
+                        result = "no correct nums";
+
                     }
+                    attempts-=1;
+                    attemptsDisplay.setText("Attempts Remaining: " + attempts);
+                    recordedAttempts.insert(0,"" + userInput[0] + userInput[1] + userInput[2] + userInput[3] + " "+ result +"\n");
+                    textRecord.setText(recordedAttempts);
                 }
-
-                if(correctPositions == 4) {
-                    textDisplay.setText("You won! pls program reset");
-                } else if(correctPositions > 0){
-                    textDisplay.setText("Player guessed a correct number. Try again");
-                } else {
-                    textDisplay.setText("No correct guesses. Try again");
-                }
-
-                attempts-=1;
-                attemptsDisplay.setText("Attempts Remaining: " + attempts);
 
                 if(attempts == 0){
                     textDisplay.setText("Gameover, secret code was " + secretCode[0]+ secretCode[1]+ secretCode[2]+ secretCode[3]+". pls reset game");
-                }
+                    initializeGame();
+                };
 
             }});
 
