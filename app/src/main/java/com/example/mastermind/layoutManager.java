bc5bb5c25fb77b483ee76de0be56cf33a6765a7a
Jason Wong <jsnwong@ucdavis.edu>
2022-04-15 00:47:47 -0700

    begin refactoring layout code to separate class

---
+++ app/src/main/java/com/example/mastermind/layoutManager.java
@@ -0,0 +1,10 @@
+package com.example.mastermind;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+public class layoutManager {
+    private Button tryButton, normalButton, mediumButton, hardButton;
+    private Button menuNormalButton, menuHardButton, menuStartButton;
+    private TextView textDisplay, attemptsDisplay, textRecord;
+}
