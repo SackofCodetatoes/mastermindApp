000d969667fce2e2250204be24833225578c845a
Jason <jsnwong@ucdavis.edu>
2022-04-16 20:59:39 -0700

    updated attempts record scrollview

--- app/src/main/java/com/example/mastermind/MainActivity.java
+++ app/src/main/java/com/example/mastermind/MainActivity.java
@@ -9,13 +9,18 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 import android.widget.NumberPicker;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    private Button tryButton, normalButton, mediumButton, hardButton, restartButton;
+    private Button tryButton, normalButton, mediumButton, hardButton, restartButton, dynamicAddButton, menuDynamicButton;
+    HorizontalScrollView scrollField;
+    ScrollView attemptsRecord;
     private Button menuNormalButton, menuHardButton, menuStartButton;
     private TextView textDisplay, attemptsDisplay, textRecord;
     NumberPicker[] numberPickers = new NumberPicker[4];
@@ -69,7 +74,8 @@ public class MainActivity extends AppCompatActivity {
 
                 break;
             case 2:
-                setContentView(R.layout.activity_gameover);
+                setContentView(R.layout.dynamic_testing);
+                initDynamicLayout();
                 //init text and buttons
                 break;
         }
@@ -106,6 +112,7 @@ public class MainActivity extends AppCompatActivity {
         //todo: simplify recorded guesses
         recordedAttempts.append("" + userInput[0] + userInput[1] + userInput[2] + userInput[3] + " "+ result +"\n");
         textRecord.setText((recordedAttempts));
+        attemptsRecord.fullScroll(View.FOCUS_DOWN);
     }
 
     void toggleButtons(int onOff){
@@ -143,28 +150,58 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
 
-            case 1:
-                //make a better ui to indicate difficult hints
-                if(gameInstance.correctPositions > 0){
-                    //color aqua
-                    for(int i = 0; i < numberPickers.length; i++) {
-                        numberPickers[i].setBackgroundColor(Color.rgb(45, 198, 207));
-                    }
-                }
-                else if(gameInstance.correctDigits > 0){
-                    //color whole wheel in orange209, 180, 48
-                    for(int i = 0; i < numberPickers.length; i++) {
-                        numberPickers[i].setBackgroundColor(Color.rgb(209, 180, 48));
-                    }
-                }
-                break;
+//            case 1:
+//                //make a better ui to indicate difficult hints
+//                if(gameInstance.correctPositions > 0){
+//                    //color aqua
+//                    for(int i = 0; i < numberPickers.length; i++) {
+//                        numberPickers[i].setBackgroundColor(Color.rgb(45, 198, 207));
+//                    }
+//                }
+//                else if(gameInstance.correctDigits > 0){
+//                    //color whole wheel in orange209, 180, 48
+//                    for(int i = 0; i < numberPickers.length; i++) {
+//                        numberPickers[i].setBackgroundColor(Color.rgb(209, 180, 48));
+//                    }
+//                }
+//                break;
         }
     }
+    void dynamicAdd(int howMany) {
+        //add a button to screen
+//        NumberPicker picker = new NumberPicker(this);
+//        String[] values = {"one", "two", "three"};
+//        picker.setMaxValue(values.length);
+//        picker.setMinValue(0);
+//        picker.setDisplayedValues(values);
 
+//        numberPickers[i] = findViewById(pickerIDs[i]);
+//        numberPickers[i].setMaxValue(7);
+//        numberPickers[i].setMinValue(0);
+//        numberPickers[i].setDisplayedValues(pickerVals);
+        Button btn = new Button(this);
+        btn.setText("Submit");
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id.dynamic_layout);
+        LinearLayout.LayoutParams buttonlayout = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        linearLayout.addView(btn, buttonlayout);
+    }
+
+
+    void initDynamicLayout(){
+        scrollField = findViewById(R.id.scroll_field);
+        dynamicAddButton = findViewById(R.id.button_menu_dynamic);
+//        dynamicAdd(2);
+        dynamicAddButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                dynamicAdd(2);
+            }
+        });
+    }
     void initMenuLayoutButtons(){
         menuStartButton = findViewById(R.id.button_menu_start);
         menuNormalButton = findViewById(R.id.button_menu_normal);
         menuHardButton = findViewById(R.id.button_menu_hard);
+        menuDynamicButton = findViewById(R.id.button_menu_dynamic);
 
         menuNormalButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
@@ -186,7 +223,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v){
                 //disable main group and enable the start screen with difficulty
                 Group menuGroup = findViewById(R.id.group_menu_screen);
-                contentViewSwitcher(1); //set view to main activity
+                contentViewSwitcher(1);
+            }
+        });
+        menuDynamicButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                contentViewSwitcher(2);
             }
         });
     }
@@ -232,7 +274,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void initGameLayoutInterface(){
-        String[] pickerVals = new String[]{"0", "1", "2", "3", "4", "5", "6", "7"};
+        String[] pickerVals = {"0", "1", "2", "3", "4", "5", "6", "7"};
         int[] pickerIDs = {R.id.numberpicker_main_picker, R.id.numberpicker_main_picker2, R.id.numberpicker_main_picker3, R.id.numberpicker_main_picker4};
         numberPickersGroup = findViewById(R.id.numberpickers_main_group);
 
@@ -242,7 +284,7 @@ public class MainActivity extends AppCompatActivity {
             numberPickers[i].setMinValue(0);
             numberPickers[i].setDisplayedValues(pickerVals);
         }
-
+        attemptsRecord = findViewById(R.id.scrollview_main_record);
         textDisplay = findViewById(R.id.text_main_display);
         textDisplay.setText(("Guess a 4 digit combination"));
         textRecord = findViewById(R.id.text_main_record);
